a793437288387ac68d4f0f514883be0bba8dc4e7
Mc kenneth <mckenneth@me.com>
2015-03-13 19:32:11 -0700

    Converted files in php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php include('header.php') ?>
+
+<main>
+    <article>
+        <header>
+            <h2>Article Title</h2>
+        </header>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta non perspiciatis odit neque, impedit cum. Nulla reprehenderit quam incidunt, asperiores ex recusandae a consequuntur ducimus error facere, officia, quasi consequatur.</p>
+    </article>
+    <article>
+        <header>
+            <h2>Article Title</h2>
+        </header>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta non perspiciatis odit neque, impedit cum. Nulla reprehenderit quam incidunt, asperiores ex recusandae a consequuntur ducimus error facere, officia, quasi consequatur.</p>
+    </article>
+    <article>
+        <header>
+            <h2>Article Title</h2>
+        </header>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta non perspiciatis odit neque, impedit cum. Nulla reprehenderit quam incidunt, asperiores ex recusandae a consequuntur ducimus error facere, officia, quasi consequatur.</p>
+    </article>
+</main>
+
+<?php include('footer.php') ?>
