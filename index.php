3e0eac1953b2c2a992603df30f1f0f9293edde8d
Mc kenneth <mckenneth@me.com>
2015-03-16 10:01:27 -0700

    Index page modified

--- index.php
+++ index.php
@@ -43,6 +43,84 @@
 			<p>As London looks back over the life and works of Alexander McQueen at the retrospective 'Savage Beauty' at the V&A, we reminisce over our own encounters with McQueen.</p>
 			<a class="button--readmore" href="single.php">Read More</a>
 		</article>
+		<article class="post" id="{pageid}">
+			<header>
+				<h2 class="h2"><a href="single.php">from beyonce to 'bruno,' the most surprising catwalk crashes</a></h2>
+			</header>
+			<p>Following Ben Stiller and Owen Wilson’s Internet-breaking trip down the Valentino runway this morning, we’ve rounded up the most epic surprise appearances and catwalk crashes of all time.</p>
+			<a class="button--readmore" href="single.php">Read More</a>
+		</article>
+
+		<article class="post" id="{pageid}">
+			<header>
+				<h2 class="h2 post__title"><a href="single.php">why glitter won’t die — dissecting an enduring trend</a></h2>
+			</header>
+			<p>The runway has spoken: glitter is back in a major way for fall. We investigate the history of sparkles in fashion and art, from Dorothy’s ruby slippers to drag queens.</p>
+			<a class="button--readmore" href="single.php">Read More</a>
+		</article>
+
+		<article class="post" id="{pageid}">
+			<header>
+				<h2 class="h2 post__title"><a href="single.php">women wear the pants at stella mccartney fall/winter 15</a></h2>
+			</header>
+			<p>Quiet female dominance exudes at Stella McCartney.</p>
+			<a class="button--readmore" href="single.php">Read More</a>
+		</article>
+
+		<article class="post" id="{pageid}">
+			<header>
+				<h2 class="h2 post__title"><a href="single.php">exclusive first look: palace spring/summer 15</a></h2>
+			</header>
+			<p>It might be sportswear, but Palace isn't playing games anymore.</p>
+			<a class="button--readmore" href="single.php">Read More</a>
+		</article>
+
+		<article class="post" id="{pageid}">
+			<header>
+				<h2 class="h2 post__title"><a href="single.php">Deconstructing beauty</a></h2>
+			</header>
+			<p>As London looks back over the life and works of Alexander McQueen at the retrospective 'Savage Beauty' at the V&A, we reminisce over our own encounters with McQueen.</p>
+			<a class="button--readmore" href="single.php">Read More</a>
+		</article>
+		<article class="post" id="{pageid}">
+			<header>
+				<h2 class="h2"><a href="single.php">from beyonce to 'bruno,' the most surprising catwalk crashes</a></h2>
+			</header>
+			<p>Following Ben Stiller and Owen Wilson’s Internet-breaking trip down the Valentino runway this morning, we’ve rounded up the most epic surprise appearances and catwalk crashes of all time.</p>
+			<a class="button--readmore" href="single.php">Read More</a>
+		</article>
+
+		<article class="post" id="{pageid}">
+			<header>
+				<h2 class="h2 post__title"><a href="single.php">why glitter won’t die — dissecting an enduring trend</a></h2>
+			</header>
+			<p>The runway has spoken: glitter is back in a major way for fall. We investigate the history of sparkles in fashion and art, from Dorothy’s ruby slippers to drag queens.</p>
+			<a class="button--readmore" href="single.php">Read More</a>
+		</article>
+
+		<article class="post" id="{pageid}">
+			<header>
+				<h2 class="h2 post__title"><a href="single.php">women wear the pants at stella mccartney fall/winter 15</a></h2>
+			</header>
+			<p>Quiet female dominance exudes at Stella McCartney.</p>
+			<a class="button--readmore" href="single.php">Read More</a>
+		</article>
+
+		<article class="post" id="{pageid}">
+			<header>
+				<h2 class="h2 post__title"><a href="single.php">exclusive first look: palace spring/summer 15</a></h2>
+			</header>
+			<p>It might be sportswear, but Palace isn't playing games anymore.</p>
+			<a class="button--readmore" href="single.php">Read More</a>
+		</article>
+
+		<article class="post" id="{pageid}">
+			<header>
+				<h2 class="h2 post__title"><a href="single.php">Deconstructing beauty</a></h2>
+			</header>
+			<p>As London looks back over the life and works of Alexander McQueen at the retrospective 'Savage Beauty' at the V&A, we reminisce over our own encounters with McQueen.</p>
+			<a class="button--readmore" href="single.php">Read More</a>
+		</article>
 
 
     </section>
