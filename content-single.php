bc9ee2cd3d36f7aa8c714d73927d301dae1b023d
Mc kenneth <mckenneth@me.com>
2015-03-15 17:23:45 -0700

    Modified Single Page Template

--- content-single.php
+++ content-single.php
@@ -1,16 +1,43 @@
 <article id="{pageid}">
         <header class="post__header">
-            <h2 class="h1 post__title">Article Title</h2>
+            <h2 class="h1 post__title">IN PRAISE OF SLOWNESS</h2>
+            <h4 class="h4">Word by <a href="#">Angelica Smith</a> &amp; Photography by <a href="#">John Hayne</a></h4>
         </header>
-        <section>
-          <img src="img/post/DSCF6411.jpg"><br/><br/>
-        </section>
-        <section class="post__text">
+
+         <p class="lead">The dream of limitless leisure persisted through the 20th century. Dazzled by the magical promise of technology, the man in the street dreamed of a life spent lounging by the pool
+        </p>
+
+        <div class="column--2">
         <p>There was a time, not so long ago, when mankind looked forward to a new Age of Leisure. Machines promised to liberate everyone from the drudgery of work. Sure, we might have put in the odd shift at the office or factory, monitoring screens, twiddling dials, signing invoices, but the rest of the day would be spent hanging out and having fun. With so much free time on our hands, words like “hurry” and “haste” would eventually fall out of the language.</p>
 
-<p>Benjamin Franklin was among the first to envision a world devoted to rest and relaxation. Inspired by the technological breakthroughs of the latter 1700s, he predicted that man would soon work no more than four hours a week. The 19th century made that prophecy look foolishly naive. In the dark satanic mills of the Industrial Revolution, men, women and even children toiled for 15 hours a day. Yet at the end of the 19th century, the Age of Leisure popped up once again on the cultural radar. George Bernard Shaw predicted that we would work two hours a day by 2000.</p>
+        <p>Benjamin Franklin was among the first to envision a world devoted to rest and relaxation. Inspired by the technological breakthroughs of the latter 1700s, he predicted that man would soon work no more than four hours a week. The 19th century made that prophecy look foolishly naive. In the dark satanic mills of the Industrial Revolution, men, women and even children toiled for 15 hours a day. Yet at the end of the 19th century, the Age of Leisure popped up once again on the cultural radar. George Bernard Shaw predicted that we would work two hours a day by 2000.</p>
 
-<p>The dream of limitless leisure persisted through the 20th century. Dazzled by the magical promise of technology, the man in the street dreamed of a life spent lounging by the pool, waited on by robots that not only mixed a mean martini but also kept the economy ticking over nicely. In 1956, Richard Nixon told Americans to prepare for a four-day workweek in the “not too distant future.” A decade later, a US Senate subcommittee heard that by 2000 Americans would be working as little as 14 hours per week. Even in the 1980s, some predicted that robotics and computers would give us all more free time than we would know what to do with.</p>
-        </section>
+        <p>The dream of limitless leisure persisted through the 20th century. Dazzled by the magical promise of technology, the man in the street dreamed of a life spent lounging by the pool, waited on by robots that not only mixed a mean martini but also kept the economy ticking over nicely. In 1956, Richard Nixon told Americans to prepare for a four-day workweek in the “not too distant future.” A decade later, a US Senate subcommittee heard that by 2000 Americans would be working as little as 14 hours per week. Even in the 1980s, some predicted that robotics and computers would give us all more free time than we would know what to do with.</p>
+        </div>
+     
+        <img src="img/post/DSCF6411.jpg">
+    
+        <div class="column--2">
+        <p>There was a time, not so long ago, when mankind looked forward to a new Age of Leisure. Machines promised to liberate everyone from the drudgery of work. Sure, we might have put in the odd shift at the office or factory, monitoring screens, twiddling dials, signing invoices, but the rest of the day would be spent hanging out and having fun. With so much free time on our hands, words like “hurry” and “haste” would eventually fall out of the language.</p>
+
+        <p>Benjamin Franklin was among the first to envision a world devoted to rest and relaxation. Inspired by the technological breakthroughs of the latter 1700s, he predicted that man would soon work no more than four hours a week. The 19th century made that prophecy look foolishly naive. In the dark satanic mills of the Industrial Revolution, men, women and even children toiled for 15 hours a day. Yet at the end of the 19th century, the Age of Leisure popped up once again on the cultural radar. George Bernard Shaw predicted that we would work two hours a day by 2000.</p>
+
+
+        <p>The dream of limitless leisure persisted through the 20th century. Dazzled by the magical promise of technology, the man in the street dreamed of a life spent lounging by the pool, waited on by robots that not only mixed a mean martini but also kept the economy ticking over nicely. In 1956, Richard Nixon told Americans to prepare for a four-day workweek in the “not too distant future.” A decade later, a US Senate subcommittee heard that by 2000 Americans would be working as little as 14 hours per week. Even in the 1980s, some predicted that robotics and computers would give us all more free time than we would know what to do with.</p>
+
+        <blockquote cite="http://example.com/facts">
+            <p>Benjamin Franklin was among the first to envision a world devoted to rest and relaxation. Inspired by the technological breakthroughs of the latter 1700s.</p>
+        </blockquote>
+        </div>
+        
+        <div class="column--2">
+         <p>The dream of limitless leisure persisted through the 20th century. Dazzled by the magical promise of technology, the man in the street dreamed of a life spent lounging by the pool, waited on by robots that not only mixed a mean martini but also kept the economy ticking over nicely. In 1956, Richard Nixon told Americans to prepare for a four-day workweek in the “not too distant future.” A decade later, a US Senate subcommittee heard that by 2000 Americans would be working as little as 14 hours per week. Even in the 1980s, some predicted that robotics and computers would give us all more free time than we would know what to do with.</p>
+
+         <img src="img/post/DSCF6462.jpg">
+
+        The dream of limitless leisure persisted through the 20th century. Dazzled by the magical promise of technology, the man in the street dreamed of a life spent lounging by the pool, waited on by robots that not only mixed a mean martini but also kept the economy ticking over nicely. In 1956, Richard Nixon told Americans to prepare for a four-day workweek in the “not too distant future.” A decade later, a US Senate subcommittee heard that by 2000 Americans would be working as little as 1
+
+        </div>
+
         
     </article>
